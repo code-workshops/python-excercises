e546965f0dfb4e4c7863061f4f09c3b49acb26ae
Proto <Protosac@users.noreply.github.com>
2017-08-03 14:44:17 -0700

    Change outcomes.

--- 04_conditionals.py
+++ 04_conditionals.py
@@ -7,28 +7,28 @@ when they turn left or give them a reward if they reach a certain score.
 import random
 
 # We'll randomly select a number from 1 to 100. The player must guess this number!
-answer = random.randrange(1,101)
+answer = random.randrange(1, 26)
 
 # Count the number of times the player gets it wrong
-tries = 0
-QUIT = False
+tries = 1
+GAME_ON = True
 
-while not QUIT:
+while GAME_ON:
     # Let the player type an answer
-    choice = input("Pick a number (1-100): ")
+    choice = input("Pick a number (1-25): ")
 
     # Complete the comparisons
     # Replace YOUR_COMPARISON_HERE with an equality
-    if YOUR_COMPARISON_HERE:
+    if tries >= 3:
+        print "Too many tries! The answer is: ", answer
+        GAME_ON = False
+    elif YOUR_COMPARISON_HERE:
         print "Too high!"
         tries += 1
     elif YOUR_COMPARISON_HERE:
         print "Too low!"
         # Increment the number of tries below
-    elif tries > 3:
-        print "Awww, too late ...the answer is:"
-        print answer
-        QUIT = True
+        tries += 1
     else:
         print "CORRECT! YOU WIN!"
-        QUIT = True
+        break
