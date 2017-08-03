8e724cd783f15166f5085470b5258a8638612b9a
Proto <Protosac@users.noreply.github.com>
2017-08-03 09:49:17 -0700

    Update 04_conditionals.py

--- 04_conditionals.py
+++ 04_conditionals.py
@@ -1,11 +1,21 @@
-tesla_speed = 90
-corvette_speed = 100
+"""
+Player Score
+
+We can give players options using conditionals and comparisons. For example, we might want the player to see certain things
+when they turn left or give them a reward if they reach a certain score.
+"""
+import random
+
+player_1 = 75
+best_score = 100
 
 # Complete the comparisons
 # Replace YOUR_COMPARISON_HERE with an equality
 if YOUR_COMPARISON_HERE:
-    print "Tesla is faster!"
+    score += 10
+    print "Player scores!"
 elif YOUR_COMPARISON_HERE:
-    print "Tesla and Corvette are equally fast!"
+    score -= 5
+    print "Wrong move!"
 else:
     print "Corvette is faster!"
