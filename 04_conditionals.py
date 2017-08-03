544fcb56b1306e7309d6769a7ad37cb93f6c2d70
Proto <Protosac@users.noreply.github.com>
2017-08-03 16:28:05 -0700

    Remove loop to break up demo of conditionals

--- 04_conditionals.py
+++ 04_conditionals.py
@@ -9,26 +9,15 @@ import random
 # We'll randomly select a number from 1 to 100. The player must guess this number!
 answer = random.randrange(1, 26)
 
-# Count the number of times the player gets it wrong
-tries = 1
-GAME_ON = True
+# Let the player type an answer
+choice = input("Pick a number (1-25): ")
 
-while GAME_ON:
-    # Let the player type an answer
-    choice = input("Pick a number (1-25): ")
-
-    # Complete the comparisons
-    # Replace YOUR_COMPARISON_HERE with an equality
-    if tries >= 3:
-        print "Too many tries! The answer is: ", answer
-        GAME_ON = False
-    elif YOUR_COMPARISON_HERE:
-        print "Too high!"
-        tries += 1
-    elif YOUR_COMPARISON_HERE:
-        print "Too low!"
-        # Increment the number of tries below
-        tries += 1
-    else:
-        print "CORRECT! YOU WIN!"
-        break
+# Complete the comparisons
+# Replace YOUR_COMPARISON_HERE with an equality
+if YOUR_COMPARISON_HERE:
+    print "Too high! You lose!"
+elif YOUR_COMPARISON_HERE:
+    print "Too low! You lose!"
+else:
+    print "CORRECT! YOU WIN!"
+    break
