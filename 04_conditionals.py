06f3762ea1226af278e1dab9571c351ebb222b61
Proto <Protosac@users.noreply.github.com>
2017-08-03 12:40:55 -0700

    Change to a random guess game.

--- 04_conditionals.py
+++ 04_conditionals.py
@@ -1,21 +1,34 @@
 """
-Player Score
+Guessing Game
 
 We can give players options using conditionals and comparisons. For example, we might want the player to see certain things
 when they turn left or give them a reward if they reach a certain score.
 """
 import random
 
-player_1 = 75
-best_score = 100
+# We'll randomly select a number from 1 to 100. The player must guess this number!
+answer = random.randrange(1,101)
 
-# Complete the comparisons
-# Replace YOUR_COMPARISON_HERE with an equality
-if YOUR_COMPARISON_HERE:
-    score += 10
-    print "Player scores!"
-elif YOUR_COMPARISON_HERE:
-    score -= 5
-    print "Wrong move!"
-else:
-    print "Corvette is faster!"
+# Count the number of times the player gets it wrong
+tries = 0
+QUIT = False
+
+while not QUIT:
+    # Let the player type an answer
+    choice = input("Pick a number (1-100): ")
+
+    # Complete the comparisons
+    # Replace YOUR_COMPARISON_HERE with an equality
+    if YOUR_COMPARISON_HERE:
+        print "Too high!"
+        tries += 1
+    elif YOUR_COMPARISON_HERE:
+        print "Too low!"
+        # Increment the number of tries below
+    elif tries > 3:
+        print "Awww, too late ...the answer is:"
+        print answer
+        QUIT = True
+    else:
+        print "CORRECT! YOU WIN!"
+        QUIT = True
