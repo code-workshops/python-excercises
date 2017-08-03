861880c5dc99e355bec4012a524cf431e5373aac
Proto <Protosac@users.noreply.github.com>
2017-08-03 08:36:30 -0700

    Create 05_conditionals.py

---
+++ 05_conditionals.py
@@ -0,0 +1,11 @@
+tesla_speed = 100
+corvette_speed = 100
+
+# Complete the comparisons
+# Replace YOUR_COMPARISON_HERE with an equality
+if YOUR_COMPARISON_HERE:
+    print "Tesla is faster!"
+elif YOUR_COMPARISON_HERE:
+    print "Tesla and Corvette are equally fast!"
+else:
+    print "Corvette is faster!"
