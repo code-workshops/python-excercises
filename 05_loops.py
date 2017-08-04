b9d377797ae205ed94782038239cb9bbdadbc093
Proto <Protosac@users.noreply.github.com>
2017-08-03 17:46:49 -0700

    Student exercise version.

--- 05_loops.py
+++ 05_loops.py
@@ -10,25 +10,24 @@ import random
 answer = random.randrange(1, 26)
 
 # Count the number of times the player gets it wrong
-tries = 1
-GAME_ON = True
+# tries = 1
+# GAME_ON = True
 
 while GAME_ON:
     # Let the player type an answer
     choice = input("Pick a number (1-25): ")
 
     # Complete the comparisons
-    # Replace YOUR_COMPARISON_HERE with an equality
-    if tries >= 3:
+    # Replace YOUR_CONDITION_HERE/YOUR_COMPARISON_HERE with an equality
+    if YOUR_CONDITION_HERE:
         print "Too many tries! The answer is: ", answer
         GAME_ON = False
     elif YOUR_COMPARISON_HERE:
         print "Too high!"
         tries += 1
     elif YOUR_COMPARISON_HERE:
-        print "Too low!"
-        # Increment the number of tries below
-        tries += 1
+        # YOUR_CODE_HERE - make a print statement that tells the player 'Too low!'
+        # YOUR_CODE_HERE - increment the tries counter
     else:
         print "CORRECT! YOU WIN!"
         break
