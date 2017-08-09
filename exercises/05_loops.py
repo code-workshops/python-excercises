2421ffdb454f3e80c1eb65b9f94cb6c081c95c92
winonatong <cutewoebot@gmail.com>
2017-08-08 20:41:38 -0500

    reorder if/elif/else block
    move check for correct answer to the beginning of the if/elif/else block so that the 'too many tries' message doesn't kick in when the correct number is guessed
    corrected the comments so the guessing range matches the code

--- exercises/05_loops.py
+++ exercises/05_loops.py
@@ -6,8 +6,8 @@ when they turn left or give them a reward if they reach a certain score.
 """
 import random
 
-# We'll randomly select a number from 1 to 100. The player must guess this number!
-answer = random.randrange(1, 26)
+# We'll randomly select a number from 1 to 25. The player must guess this number!
+answer = random.randrange(1, 25)
 
 # Count the number of times the player gets it wrong
 tries = 1
@@ -15,20 +15,20 @@ GAME_ON = True
 
 while GAME_ON:
     # Let the player type an answer
-    choice = input("Pick a number (1-25): ")
+    choice = int(input("Pick a number (1-25): "))
 
     # Complete the comparisons
     # Replace YOUR_COMPARISON_HERE with an equality
-    if tries >= 3:
+    if answer == choice:
+        print "CORRECT! YOU WIN!"
+        break
+    elif tries >= 3:
         print "Too many tries! The answer is: ", answer
         GAME_ON = False
-    elif YOUR_COMPARISON_HERE:
+    elif choice > answer:
         print "Too high!"
         tries += 1
-    elif YOUR_COMPARISON_HERE:
+    else:
         print "Too low!"
         # Increment the number of tries below
         tries += 1
-    else:
-        print "CORRECT! YOU WIN!"
-        break
