0fd84b82c2d55da1eae1dd1d15dc4a970438c28e
Proto <Protosac@users.noreply.github.com>
2017-08-09 19:02:11 -0700

    Create 06_functions.py

---
+++ exercises/06_functions.py
@@ -0,0 +1,12 @@
+"""
+Practice creating functions!
+"""
+
+def send(message):
+    """Return the given message."""
+    return
+    
+def double(number):
+    """Double the number and return the product."""
+    return
+
